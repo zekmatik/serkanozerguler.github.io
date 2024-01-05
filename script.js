8e4aac1f4d3a07de9de1b57a0f0d582e76738da1
Serkan Güler <133968933+zekmatik@users.noreply.github.com>
2024-01-05 18:25:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// function changeBackgroundColor() {
+//     var randomColor = getRandomColor();
+//     document.body.style.backgroundColor = randomColor;
+// }
+
+// function getRandomColor() {
+//     var letters = '0123456789ABCDEF';
+//     var color = '#';
+//     for (var i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
